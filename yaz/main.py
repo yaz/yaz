0f926c5dc5b066af4dfd62679b59602e1a73ece8
Evi Young <eviy@frayja.com>
2017-01-02 14:40:39 +0100

    Remove debug code

--- yaz/main.py
+++ yaz/main.py
@@ -5,23 +5,9 @@ from .plugin import Plugin
 from .parser import Parser
 
 def main():
-    # tasks = get_task_list()
-    # print(tasks)
-
-    tasks = get_task_tree()
-    print(tasks)
-
-    # plugins = Plugin.get_yaz_plugin_list()
-    # print(plugins)
-
     parser = Parser()
-    # print(parser)
+    parser.add_task_tree(get_task_tree())
 
-    parser.add_task_tree(tasks)
     task, kwargs = parser.parse_arguments(sys.argv[1:])
-    print(task, kwargs)
-
     if task:
         print(task(**kwargs))
-
-    print("--------------------------------------------------------------------------------")
