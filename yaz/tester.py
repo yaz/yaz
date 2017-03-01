85badabba096b3e9ec306bccc41218c32449db7f
boudewijn <boudewijn@sniper.frayja.com>
2017-03-01 19:47:27 +0100

    Use logger instead of print

--- yaz/tester.py
+++ yaz/tester.py
@@ -4,13 +4,15 @@ import unittest
 from yaz.parser import Parser
 from yaz.task import get_task_tree
 
+from .log import logger
+
 
 class TestCase(unittest.TestCase):
     def get_caller(self, white_list=None):
         def caller(*arguments):
             try:
                 task, kwargs = parser.parse_arguments([sys.argv[0]] + list(arguments))
-                print("Calling", task, "with", kwargs)
+                logger.debug("Calling %s with %s", task, kwargs)
 
                 self.assertTrue(callable(task), "The parser did not find a task")
                 self.assertTrue(isinstance(kwargs, dict), "The parser did not find the parameters")
