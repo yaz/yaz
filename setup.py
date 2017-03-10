748d48333673c61278b0dbb08732a11ba11c2232
boudewijn <boudewijn@sniper.frayja.com>
2017-03-10 14:40:11 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
-from setuptools import setup
+import os
+import setuptools
+import sys
 
-setup(name="yaz",
+version = "1.0.2"
+
+if sys.argv[-1] == "tag":
+    os.system("git tag -a {} -m \"Release {}\"".format(version, version))
+    os.system("git push origin {}".format(version))
+    sys.exit()
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now")
+    sys.exit()
+
+setuptools.setup(name="yaz",
       packages=["yaz"],
-      version="1.0.1",
+      version=version,
       description="A scripting tool",
       author="Boudewijn Schoon",
       author_email="yaz@frayja.com",
@@ -10,7 +25,7 @@ setup(name="yaz",
       license="MIT",
       zip_safe=False,
       test_suite="nose.collector",
-      tests_require=["nose"],
+      tests_require=["nose", "coverage"],
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Environment :: Console",
