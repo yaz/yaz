ac5d78cebf99072da6de95e192906207bb4e7eb3
boudewijn <boudewijn@sniper.frayja.com>
2017-04-02 09:56:14 +0200

    Remove comment from publication helper

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ if sys.argv[-1] == "tag":
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     os.system("python setup.py bdist_wheel upload")
-    print("You probably want to also tag the version now")
     sys.exit()
 
 setuptools.setup(name="yaz",
